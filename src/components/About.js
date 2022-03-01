3583812fbb1397359cf325ee3c507224b7da70b7
Christian Castillo <chriscast94@outlook.com>
2022-03-01 12:58:17 -0800

    updated sizing

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text 3xl mb-4 font-medium text-white">
-                        Salutations! My name is Christian Castillo
+                        Salutations! I'm Christian Castillo, a writer, editor, and full-stack developer.
                         <br className="hidden lg:inline-block" /> Let's create something together.
                     </h1>
                     <p className="mb-8 leading-relaxed">
@@ -18,7 +18,7 @@ export default function About() {
                         </a>
                     </div>
                 </div>
-                <div className="lg:max-lg lg:w-full md: w-1/2 w5/6">
+                <div className="lg:max-lg lg:w-fit md: w-1/2 w5/6">
                     {/* hero image */}
                     <img className="object-cover object-center rounded" alt="hero" src="../hero.png" />
                 </div>
